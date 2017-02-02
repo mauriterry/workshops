3640b5e0128c656b2854c08b11fdd4c6e8360402
Diego Alberto Rincón Yáñez <d1egoprog@users.noreply.github.com>
2017-02-02 18:36:08 -0500

    Update mysql-5.6 on CentOS7.0.sh

--- Linux-Services-Recipies/mysql-5.6 on CentOS7.0.sh
+++ Linux-Services-Recipies/mysql-5.6 on CentOS7.0.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo yum update
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 sudo rpm -ivh mysql-community-release-el7-5.noarch.rpm
-yum update
 sudo yum install mysql-server
 sudo systemctl start mysqld
 sudo mysql_secure_installation
